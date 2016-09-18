b068a135013e1faa02bad6f563e78eebe1208418
SHTrassEr <SHTrassEr@gmail.com>
2016-09-18 18:06:31 +0800

    IPlayerAction

---
+++ STSEngine/Action/Impl/PlayerActionImpl.ts
@@ -0,0 +1,76 @@
+﻿module STSEngine {
+    "use strict";
+
+    export class PlayerActionImpl implements IPlayerAction {
+
+        protected commandListService: ICommandListService;
+        protected playerId: number;
+
+        constructor(playerId: number, commandListService: ICommandListService) {
+            this.playerId = playerId;
+            this.commandListService = commandListService;
+        }
+
+        public getPlayerId(): number {
+            return this.playerId;
+        }
+
+        protected createAttributeList(): IKeyValuePair[] {
+            return [];
+        }
+
+        protected addObjectIdAttribute(attributeList: IKeyValuePair[], objectId: number): void {
+            attributeList.push(new KeyValuePairImpl(AttributeType.ObjectId, objectId));
+        }
+
+        public startMoveRight(objectId: number): void {
+            var attributeList = this.createAttributeList();
+            this.addObjectIdAttribute(attributeList, objectId);
+            this.commandListService.createCommand(STSEngine.CommandType.StartMoveRight, this.playerId, attributeList);
+        }
+
+        public startMoveLeft(objectId: number): void {
+            var attributeList = this.createAttributeList();
+            this.addObjectIdAttribute(attributeList, objectId);
+            this.commandListService.createCommand(STSEngine.CommandType.StartMoveLeft, this.playerId, attributeList);
+        }
+
+        public startMoveUp(objectId: number): void {
+            var attributeList = this.createAttributeList();
+            this.addObjectIdAttribute(attributeList, objectId);
+            this.commandListService.createCommand(STSEngine.CommandType.StartMoveUp, this.playerId, attributeList);
+        }
+
+        public startMoveDown(objectId: number): void {
+            var attributeList = this.createAttributeList();
+            this.addObjectIdAttribute(attributeList, objectId);
+            this.commandListService.createCommand(STSEngine.CommandType.StartMoveDown, this.playerId, attributeList);
+        }
+
+        public stopMoveRight(objectId: number): void {
+            var attributeList = this.createAttributeList();
+            this.addObjectIdAttribute(attributeList, objectId);
+            this.commandListService.createCommand(STSEngine.CommandType.StopMoveRight, this.playerId, attributeList);
+        }
+
+        public stopMoveLeft(objectId: number): void {
+            var attributeList = this.createAttributeList();
+            this.addObjectIdAttribute(attributeList, objectId);
+            this.commandListService.createCommand(STSEngine.CommandType.StopMoveLeft, this.playerId, attributeList);
+        }
+
+        public stopMoveUp(objectId: number): void {
+            var attributeList = this.createAttributeList();
+            this.addObjectIdAttribute(attributeList, objectId);
+            this.commandListService.createCommand(STSEngine.CommandType.StopMoveUp, this.playerId, attributeList);
+        }
+
+        public stopMoveDown(objectId: number): void {
+            var attributeList = this.createAttributeList();
+            this.addObjectIdAttribute(attributeList, objectId);
+            this.commandListService.createCommand(STSEngine.CommandType.StopMoveDown, this.playerId, attributeList);
+        }
+
+
+    }
+}
--- STSEngine/app.ts
+++ STSEngine/app.ts
@@ -10,35 +10,26 @@
     var world: STSEngine.IWorld = new STSEngine.WorldImpl(worldSettings, objectListService, processListService);
     var engine: STSEngine.IEngine = new STSEngine.EngineImpl(world, commandListService);
 
+    var playerAction = new STSEngine.PlayerActionImpl(1, commandListService);
+
+
+
+
+    engine.step();
 
     var registerPlayerAttributeList: STSEngine.IKeyValuePair[] = [];
     registerPlayerAttributeList.push(new STSEngine.KeyValuePairImpl(STSEngine.AttributeType.NewPlayerId, 1));
-
-    var moveDownCommandAttributeList = new Map<string, any>();
-    moveDownCommandAttributeList.set(STSEngine.AttributeType.ObjectId, 1);
-
-    var stopMoveDownCommandAttributeList = new Map<string, any>();
-    stopMoveDownCommandAttributeList.set(STSEngine.AttributeType.ObjectId, 1);
-
-
-    var moveRightCommandAttributeList = new Map<string, any>();
-    moveRightCommandAttributeList.set(STSEngine.AttributeType.ObjectId, 1);
-
-    var stopMoveRightCommandAttributeList = new Map<string, any>();
-    stopMoveRightCommandAttributeList.set(STSEngine.AttributeType.ObjectId, 1);
-
-
-    engine.step();
     commandListService.createCommand(STSEngine.CommandType.RegisterPlayer, 0, registerPlayerAttributeList);
-    commandListService.createCommand(STSEngine.CommandType.StartMoveDown, 1, moveDownCommandAttributeList);
+
+    playerAction.startMoveDown(1);
     engine.step();
     engine.step();
 
-    commandListService.createCommand(STSEngine.CommandType.StartMoveRight, 1, moveRightCommandAttributeList);
+    playerAction.startMoveRight(1);
     engine.step();
     engine.step();
 
-    commandListService.createCommand(STSEngine.CommandType.StopMoveDown, 1, moveDownCommandAttributeList);
+    playerAction.stopMoveDown(1);
     engine.step();
 
     engine.step();
@@ -47,7 +38,7 @@
     engine.step();
     engine.step();
 
-    commandListService.createCommand(STSEngine.CommandType.StopMoveRight, 1, moveDownCommandAttributeList);
+    playerAction.stopMoveRight(1);
     engine.step();
 
     var o = objectListService.getObject(1);
